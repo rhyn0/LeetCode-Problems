0481cb63c1814c9cf5c1fbf5c9fb4c27f675621c
Ryan Ozawa <ryan.ozawa21@gmail.com>
2023-05-10 00:33:46 -0700

    start String section of DP essentials Study plan

--- study_plans/essential_dynamic_prog/long_palindrome_str.py
+++ study_plans/essential_dynamic_prog/long_palindrome_str.py
@@ -3,7 +3,7 @@ import doctest
 
 
 class Solution:  # noqa: D101
-    def longestPalindrome(self, in_str: str) -> str:  # spec
+    def longestPalindrome(self, in_str: str) -> str:
         """Return longest palindromic substring in given string.
 
         Args:
@@ -14,21 +14,20 @@ class Solution:  # noqa: D101
         """
         long_len, long_start, long_end = 0, 0, 0
         size_n = len(in_str)
-        for center in range(size_n):
-            left = right = center
+
+        def find_palindrome(left: int, right: int) -> None:
+            nonlocal long_len, long_start, long_end
             while 0 <= left <= right < size_n and in_str[left] == in_str[right]:
                 if right - left + 1 > long_len:
                     long_len, long_start, long_end = right - left + 1, left, right
                 left -= 1
                 right += 1
 
+        for center in range(size_n):
+            find_palindrome(center, center)
             # even len
-            left, right = center, center + 1
-            while 0 <= left <= right < size_n and in_str[left] == in_str[right]:
-                if right - left + 1 > long_len:
-                    long_len, long_start, long_end = right - left + 1, left, right
-                left -= 1
-                right += 1
+            find_palindrome(center, center + 1)
+
         return in_str[long_start : long_end + 1]
 
 
@@ -39,16 +38,22 @@ def main():
 
     Setup:
         >>> sol = Solution()
+        >>> example_case_1 = "babad"
+        >>> example_case_2 = "cbbd"
 
     Example 1:
-        >>> sol.longestPalindrome("babad")
+        >>> sol.longestPalindrome(example_case_1)
         'bab'
 
     Example 2:
-        >>> sol.longestPalindrome("cbbd")
+        >>> sol.longestPalindrome(example_case_2)
         'bb'
     """
 
 
 if __name__ == "__main__":
-    doctest.testmod(verbose=True)
+    doctest.testmod(
+        optionflags=doctest.REPORTING_FLAGS ^ doctest.FAIL_FAST
+        | doctest.ELLIPSIS
+        | doctest.NORMALIZE_WHITESPACE
+    )
