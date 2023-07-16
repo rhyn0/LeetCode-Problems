53602681aee542c87874a259b5dfffde0a76edfa
Ryan Ozawa <ryan.ozawa21@gmail.com>
2023-07-16 11:39:04 -0700

    checkpoint for july week 2

---
+++ daily_challenges/july/course_schedule.py
@@ -0,0 +1,66 @@
+"""Daily Challenge for July 13, 2023 on LeetCode."""
+# Standard Library
+from collections import defaultdict
+from collections import deque
+import doctest
+
+
+class Solution:  # noqa: D101
+    def canFinish(self, num_courses: int, prereq: list[list[int]]) -> bool:
+        """Return if possible to complete all courses with the given prerequisites.
+
+        Prerequisite list is of form [a_i, b_i] where b_i must be taken before a_i.
+        Courses are numbered from [0, num_courses)
+
+        Args:
+            num_courses (int): Total number of courses
+            prereq (list[list[int]]): List of prerequisites
+
+        Returns:
+            bool: True if all courses can be completed, False otherwise
+        """
+        num_inbound_edges = defaultdict(int)
+        node_neighbors: defaultdict[int, list[int]] = defaultdict(list)
+        for to_node, from_node in prereq:
+            node_neighbors[from_node].append(to_node)
+            num_inbound_edges[to_node] += 1
+
+        num_visited_nodes = 0
+        q = deque(node for node in range(num_courses) if num_inbound_edges[node] == 0)
+        while q:
+            num_visited_nodes += 1
+            curr_node = q.popleft()
+            for neighbor in node_neighbors[curr_node]:
+                num_inbound_edges[neighbor] -= 1
+                if num_inbound_edges[neighbor] == 0:
+                    q.append(neighbor)
+
+        return num_courses == num_visited_nodes
+
+
+def main() -> None:
+    """207. Course Schedule on LeetCode.
+
+    ====================================================
+
+    Setup:
+        >>> sol = Solution()
+        >>> example_case_1 = 2, [[1,0]]
+        >>> example_case_2 = 2, [[1,0],[0,1]]
+
+    Example 1:
+        >>> sol.canFinish(*example_case_1)
+        True
+
+    Example 2:
+        >>> sol.canFinish(*example_case_2)
+        False
+    """
+
+
+if __name__ == "__main__":
+    doctest.testmod(
+        optionflags=doctest.REPORTING_FLAGS ^ doctest.FAIL_FAST
+        | doctest.ELLIPSIS
+        | doctest.NORMALIZE_WHITESPACE,
+    )
---
+++ weekly_challenges/july/substr_k_distinct_chars.py
@@ -0,0 +1,62 @@
+"""Weekly Challenge for 2nd Week of July, 2023 on LeetCode."""
+# Standard Library
+from collections import Counter
+import doctest
+
+
+class Solution:  # noqa: D101
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        """Return length of longest substring with at most k distinct characters.
+
+        Args:
+            s (str): String to search in
+            k (int): Maximum number of distinct characters
+
+        Returns:
+            int: Longest length
+        """
+        if k == 0:
+            return 0
+        n = len(s)
+        left = 0
+        seen_chars = Counter(s[:k])
+        longest_len = seen_chars.total()
+        for right in range(k, n):
+            seen_chars[s[right]] += 1
+            while len(seen_chars) > k:
+                if seen_chars[s[left]] == 1:
+                    seen_chars.pop(s[left])
+                else:
+                    seen_chars[s[left]] -= 1
+                left += 1
+            longest_len = max(longest_len, seen_chars.total())
+
+        return longest_len
+
+
+def main() -> None:
+    """340. Longest Substring with At Most K Distinct Characters on LeetCode.
+
+    ====================================================
+
+    Setup:
+        >>> sol = Solution()
+        >>> example_case_1 = "eceba", 2
+        >>> example_case_2 = "aa", 1
+
+    Example 1:
+        >>> sol.lengthOfLongestSubstringKDistinct(*example_case_1)
+        3
+
+    Example 2:
+        >>> sol.lengthOfLongestSubstringKDistinct(*example_case_2)
+        2
+    """
+
+
+if __name__ == "__main__":
+    doctest.testmod(
+        optionflags=doctest.REPORTING_FLAGS ^ doctest.FAIL_FAST
+        | doctest.ELLIPSIS
+        | doctest.NORMALIZE_WHITESPACE,
+    )
